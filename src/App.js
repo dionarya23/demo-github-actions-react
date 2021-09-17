8bc2ef853e0bdf78406bc4cac9db854c256cabf1
Dion Arya Pamungkas <dionarya.p@gmail.com>
2021-09-17 16:11:11 +0700

    hello world

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       Mantap Jiwa
+       Hello Jiwa
         </p>
         <a
           className="App-link"
